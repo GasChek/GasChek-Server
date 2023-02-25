02738dae5ce19ae0d772caefcfde81ee6dee52e2
unknown <mimarkethq@gmail.com>
2023-02-25 03:46:44 +0100

    first commit

--- gaschek_backend/settings.py
+++ gaschek_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SERVER_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gaschek.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
