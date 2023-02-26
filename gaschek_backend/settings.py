c2a145fa991944e9d87530c6a9448f19bab7ed25
unknown <mimarkethq@gmail.com>
2023-02-26 22:54:02 +0100

    added cors origin whitelist

--- gaschek_backend/settings.py
+++ gaschek_backend/settings.py
@@ -160,7 +160,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ORIGIN_WHITELIST = [
     'https://gaschek.netlify.app',
 ]
-CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+CORS_ALLOW_METHODS = ['GET', 'POST', 'PATCH', 'OPTIONS']
 CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
