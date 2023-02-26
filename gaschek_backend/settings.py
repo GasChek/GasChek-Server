05badbc73cd5ffaad157b9d5534221432e2d246f
unknown <mimarkethq@gmail.com>
2023-02-26 23:03:11 +0100

    added cors origin whitelist

--- gaschek_backend/settings.py
+++ gaschek_backend/settings.py
@@ -28,11 +28,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SERVER_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['https://gaschek.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -153,15 +151,16 @@ REST_FRAMEWORK = {
     )
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://gaschek.herokuapp.com']
+
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'http://localhost:3000',
-    'http://192.168.43.102:3000',
     'https://gaschek.herokuapp.com',
     'https://gaschek.netlify.app'
 ]
+CORS_ORIGIN_WHITELIST = [
+    'https://gaschek.netlify.app',
+]
 
-# CORS_ALLOWED_ORIGINS_ALL = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -171,11 +170,14 @@ EMAIL_HOST_USER = 'gaschektechnology@gmail.com'
 EMAIL_HOST_PASSWORD = 'wpjzmukkuxrrgmcn'
 EMAIL_USE_SSL = True
 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
 
 # REDIS CONFIGURATIONS
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 0
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+# REDIS_DB = 0
 
 # Configure Django to use Redis as a message broker
 # CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
@@ -216,5 +218,3 @@ REDIS_DB = 0
 # #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
