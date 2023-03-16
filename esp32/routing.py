7508fc034caa00c64eb154e676070e17181c6f80
unknown <michaelibeh12.com>
2023-03-16 11:53:49 +0100

    change

--- esp32/routing.py
+++ esp32/routing.py
@@ -22,7 +22,6 @@ urlpatterns = [
     re_path('ws/toggle_details/', ToggleGasDetailsConsumer.as_asgi()),
     re_path('ws/get_device_details/', GasDetailsDeviceConsumer.as_asgi()),
     re_path('ws/send_device_details/', SendDeviceDetailsConsumer.as_asgi()),
-\
     re_path('ws_order/get_dealer_orders/', GasDealerOrdersConsumer.as_asgi()),
     re_path('ws/get_gas_leakage/', GasLeakageNotificationConsumer.as_asgi())
 ]
