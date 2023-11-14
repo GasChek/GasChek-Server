5b61731f80f53c9e691981fa1a16d280f3570e58
unknown <michaelibeh12@gmail.com>
2023-11-14 15:23:24 +0100

    improved websocket codes

--- esp32/consumers.py
+++ esp32/consumers.py
@@ -15,31 +15,25 @@ load_dotenv()
 # MQTT
 
 
-def generate_random_text(length=10):
-    characters = string.ascii_letters + string.digits
-    random_text = ''.join(random.choice(characters) for i in range(length))
-    return random_text
-        
-def connect_to_mqtt_broker(topic):
-    client = paho.Client(client_id=f'mqtt-GasChek-client-{generate_random_text()}', userdata=None, protocol=paho.MQTTv5)
-    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-    client.username_pw_set(str(os.getenv('MQTT_USER')), str(os.getenv('MQTT_PASSWORD')))
-    client.connect(str(os.getenv('MQTT_SERVER_URL')), int(os.getenv('MQTT_PORT')))
-    client.subscribe(str(topic), qos=1)
-    return client
-
 class GasDetailsConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.token_id = None
+        self.client = None
+
     def connect(self):
-        token = self.scope['query_string'].decode('utf-8')
         try:
+            token = self.scope['query_string'].decode('utf-8')
             payload = jwt_decoder(token)
             self.token_id = payload['id']
-            self.client = connect_to_mqtt_broker(self.token_id)
+            self.client = self.connect_to_mqtt_broker(self.token_id)
             self.accept()
         except Exception:
             self.close()
 
     def disconnect(self, close_code):
+        post_save.disconnect(self.device_save, sender=Gaschek_Device)
+        post_save.disconnect(self.user_save, sender=User)
         self.client.unsubscribe(str(self.token_id))
         self.client.loop_stop()
         self.close()
@@ -50,123 +44,107 @@ class GasDetailsConsumer(WebsocketConsumer):
             decrypted_text_data = decrypt(convert_byte_to_text)
             client_data = json.loads(decrypted_text_data)
 
-            def get_device():
-                data = Gaschek_Device.objects.get(user=self.token_id)
-                return data
-
-            def send_data():
-                serializer = Gaschek_Device_Serializer(get_device())
-                encrypted_data = encrypt(json.dumps(serializer.data))
-
-                data = encrypt(json.dumps({
-                    'msg': encrypted_data
-                })).encode('utf-8')
-                self.send(bytes_data=data)
-
-            def publish_data_to_mqtt():
-                user = User.objects.get(id=self.token_id)
-                gaschek_device = get_device()
-
-                payload = json.dumps({
-                    'alarm': gaschek_device.alarm,
-                    'call': gaschek_device.call,
-                    'text': gaschek_device.text,
-                    'indicator': gaschek_device.indicator,
-                    'country_code': user.country_code,
-                    'number_one': user.phonenumber_ordering,
-                    'number_two': user.phonenumber_gaschek_device_1,
-                    'number_three': user.phonenumber_gaschek_device_2,
-                    'number_four': user.phonenumber_gaschek_device_3
-                })
-                self.client.publish(str(self.token_id), payload=payload, qos=0)
-  
-            def receive_data_from_mqtt():
-                def on_message(client, userdata, msg):
-                    device_data = json.loads(msg.payload.decode('utf-8'))
-                    if "cylinder" in device_data:
-                        device = get_device()
-                        device.cylinder = device_data["cylinder"]
-                        device.gas_mass = device_data["gas_mass"]
-                        device.gas_level = device_data["gas_level"]
-                        device.battery_level = device_data["battery_level"]
-                        device.indicator = device_data["indicator"]
-                        device.save()
-                self.client.on_message = on_message
-                self.client.loop_start()
-
             if client_data["action"] == 0:
-                receive_data_from_mqtt()
-                send_data()
-
-                #signals
-                def device_save(instance, **kwargs):
-                    if (instance.user.id == self.token_id):
-                        send_data()
-                post_save.connect(device_save, sender=Gaschek_Device, weak=False)
-
-                def user_save(instance, **kwargs):
-                    if (instance.id == self.token_id):
-                        publish_data_to_mqtt()
-                post_save.connect(user_save, sender=User, weak=False)
-                #signals
-
-            data2 = encrypt(json.dumps({
-                'msg': 400
-            })).encode('utf-8')
-            
-            if (client_data["action"] == 'a'):
-                try:
-                    data = get_device()
-                    if (data.alarm == "on"):
-                        data.alarm = "off"
-                    else:
-                        data.alarm = "on"
-                    data.save()
-                    publish_data_to_mqtt()
-                except Exception:
-                    self.send(bytes_data=data2)
-
-            if (client_data["action"] == 'c'):
-                try:
-                    data = get_device()
-                    if (data.call == "on"):
-                        data.call = "off"
-                    else:
-                        data.call = "on"
-                    data.save()
-                    publish_data_to_mqtt()
-                except Exception:
-                    self.send(bytes_data=data2)
-
-            if (client_data["action"] == 't'):
-                try:
-                    data = get_device()
-                    if (data.text == "on"):
-                        data.text = "off"
-                    else:
-                        data.text = "on"
-                    data.save()
-                    publish_data_to_mqtt()
-                except Exception:
-                    self.send(bytes_data=data2)
-
-            if (client_data["action"] == 'i'):
-                try:
-                    data = get_device()
-                    if (data.indicator == "on"):
-                        data.indicator = "off"
-                    else:
-                        data.indicator = "on"
-                    data.save()
-                    publish_data_to_mqtt()
-                except Exception:
-                    self.send(bytes_data=data2)
+                self.receive_data_from_mqtt()
+                self.send_data()
+                self.setup_signals()
+            else:
+                self.handle_client_action(client_data)
 
         except Exception:
             self.send(bytes_data=encrypt(json.dumps({
                 'msg': 400
             })).encode('utf-8'))
+    
+    def generate_random_text(self, length=10):
+        characters = string.ascii_letters + string.digits
+        random_text = ''.join(random.choice(characters) for i in range(length))
+        return random_text
+        
+    def connect_to_mqtt_broker(self, topic):
+        client = paho.Client(client_id=f'mqtt-GasChek-client-{self.generate_random_text()}', userdata=None, protocol=paho.MQTTv5)
+        client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+        client.username_pw_set(str(os.getenv('MQTT_USER')), str(os.getenv('MQTT_PASSWORD')))
+        client.connect(str(os.getenv('MQTT_SERVER_URL')), int(os.getenv('MQTT_PORT')))
+        client.subscribe(str(topic), qos=1)
+        return client
 
+    def get_device(self):
+        return Gaschek_Device.objects.get(user=self.token_id)
+             
+    def send_data(self):
+        serializer = Gaschek_Device_Serializer(self.get_device())
+        encrypted_data = encrypt(json.dumps(serializer.data))
+
+        data = encrypt(json.dumps({
+            'msg': encrypted_data
+        })).encode('utf-8')
+        self.send(bytes_data=data)
+
+    def device_save(self, instance, **kwargs):
+        if (instance.user.id == self.token_id):
+            self.send_data()
+
+    def user_save(self, instance, **kwargs):
+        if (instance.id == self.token_id):
+            self.publish_data_to_mqtt()
+
+    def setup_signals(self):
+        post_save.connect(self.device_save, sender=Gaschek_Device, weak=False)
+        post_save.connect(self.user_save, sender=User, weak=False)
+
+    def publish_data_to_mqtt(self):
+        user = User.objects.get(id=self.token_id)
+        gaschek_device = self.get_device()
+
+        payload = json.dumps({
+            'alarm': gaschek_device.alarm,
+            'call': gaschek_device.call,
+            'text': gaschek_device.text,
+            'indicator': gaschek_device.indicator,
+            'country_code': user.country_code,
+            'number_one': user.phonenumber_ordering,
+            'number_two': user.phonenumber_gaschek_device_1,
+            'number_three': user.phonenumber_gaschek_device_2,
+            'number_four': user.phonenumber_gaschek_device_3
+        })
+        self.client.publish(str(self.token_id), payload=payload, qos=0)
+
+    def receive_data_from_mqtt(self):
+        def on_message(client, userdata, msg):
+            device_data = json.loads(msg.payload.decode('utf-8'))
+            if "cylinder" in device_data:
+                device = self.get_device()
+                device.cylinder = device_data["cylinder"]
+                device.gas_mass = device_data["gas_mass"]
+                device.gas_level = device_data["gas_level"]
+                device.battery_level = device_data["battery_level"]
+                device.indicator = device_data["indicator"]
+                device.save()
+        self.client.on_message = on_message
+        self.client.loop_start()
+
+    def toggle_device_state(self, attribute):
+        try:
+            data = self.get_device()
+            setattr(data, attribute, 'off' if getattr(data, attribute) == 'on' else 'on')
+            data.save()
+            self.publish_data_to_mqtt()
+        except Exception:
+            self.send(bytes_data= encrypt(json.dumps({
+                'msg': 400
+            })).encode('utf-8'))
+
+    def handle_client_action(self, client_data):
+        action_to_attribute = {
+            'a': 'alarm',
+            'c': 'call',
+            't': 'text',
+            'i': 'indicator'
+        }
+        action = client_data["action"]
+        if action in action_to_attribute:
+            self.toggle_device_state(action_to_attribute[action])
 
 # class ToggleDetailsConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
